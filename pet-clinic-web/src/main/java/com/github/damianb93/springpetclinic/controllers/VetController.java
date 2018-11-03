49462242eff87d0d6083ec0d0f99bdfd797ee0bc
damianb93 <damianburak93@gmail.com>
2018-11-03 15:39:35 +0100

    feat: Add list of vets #15

--- pet-clinic-web/src/main/java/com/github/damianb93/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/github/damianb93/springpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.github.damianb93.springpetclinic.controllers;
 
+import com.github.damianb93.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("vets")
 public class VetController {
 
-    @GetMapping({"", "index", "index.html"})
-    public String listVets() {
-        return "vets/index";
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
     }
 
+    @GetMapping({"", "index", "index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
+        return "vets/index";
+    }
 }
