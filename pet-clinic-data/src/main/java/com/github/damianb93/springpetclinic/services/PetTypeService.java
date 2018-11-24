ee80294e71010613592891a5af24664c93435325
damianb93 <damianburak93@gmail.com>
2018-11-24 11:43:42 +0100

    feat: Implement PetType service #29

---
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.github.damianb93.springpetclinic.services;
+
+import com.github.damianb93.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
