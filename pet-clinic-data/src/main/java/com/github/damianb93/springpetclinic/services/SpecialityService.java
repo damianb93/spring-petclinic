cf82518c5000fd0144c074eef87d932e827a4c5a
damianb93 <damianburak93@gmail.com>
2018-11-24 12:52:52 +0100

    feat: Implement speciality service #31

---
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.github.damianb93.springpetclinic.services;
+
+import com.github.damianb93.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/services/map/SpecialityServiceMapImpl.java
@@ -0,0 +1,36 @@
+package com.github.damianb93.springpetclinic.services.map;
+
+import com.github.damianb93.springpetclinic.model.Speciality;
+import com.github.damianb93.springpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMapImpl extends AbstractMapService<Speciality> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
