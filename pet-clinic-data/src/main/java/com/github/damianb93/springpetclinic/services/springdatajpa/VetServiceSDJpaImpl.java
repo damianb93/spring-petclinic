08d571eb2c69074eb2284e0f2cbadffb0781fb96
damianb93 <damianburak93@gmail.com>
2019-01-20 11:05:06 +0100

    feat: Implement Spring Data Jpa Vet service #41

---
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/services/springdatajpa/VetServiceSDJpaImpl.java
@@ -0,0 +1,48 @@
+package com.github.damianb93.springpetclinic.services.springdatajpa;
+
+import com.github.damianb93.springpetclinic.model.Vet;
+import com.github.damianb93.springpetclinic.repositories.VetRepository;
+import com.github.damianb93.springpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetServiceSDJpaImpl implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceSDJpaImpl(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
