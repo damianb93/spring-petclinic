0c6a5949d00b6b5007657895b8b37ac6d1f304d6
damianb93 <damianburak93@gmail.com>
2019-01-20 11:49:00 +0100

    feat: Implement Map Visit service #46

---
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.github.damianb93.springpetclinic.services;
+
+import com.github.damianb93.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/services/map/VisitServiceMapImpl.java
@@ -0,0 +1,42 @@
+package com.github.damianb93.springpetclinic.services.map;
+
+import com.github.damianb93.springpetclinic.model.Visit;
+import com.github.damianb93.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMapImpl extends AbstractMapService<Visit> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getPetType() == null ||
+            object.getPet().getId() == null || object.getPet().getOwner().getId() == null ||
+            object.getPet().getPetType().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
