18d49d300f1669d739fa21dab7d6faa5bba4ea8b
damianb93 <damianburak93@gmail.com>
2019-01-20 10:24:28 +0100

    feat: Implement Visit entity #38

--- pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/github/damianb93/springpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.github.damianb93.springpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
