7fcfff87569aa8fca5187a8135f18963f3caef00
damianb93 <damianburak93@gmail.com>
2019-03-02 11:22:09 +0100

    feat: Implement vets json endpoint #64

--- pet-clinic-web/src/main/java/com/github/damianb93/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/github/damianb93/springpetclinic/controllers/VetController.java
@@ -1,10 +1,14 @@
 package com.github.damianb93.springpetclinic.controllers;
 
+import com.github.damianb93.springpetclinic.model.Vet;
 import com.github.damianb93.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping({"vets", "vets.html"})
@@ -22,5 +26,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("api")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
